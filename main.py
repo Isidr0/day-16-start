d20a087211a1cf4091d67de77180fa77b82bf1d5
Isidr0 <rtwatson86@gmail.com>
2021-11-14 15:02:52 -0500

    updated day 16

--- main.py
+++ main.py
@@ -4,23 +4,35 @@
 # #tapped into object another_module and called variable (V icon) another_variable
 # print(another_module.another_variable)
 
-#import turtle
-#from turtle module import Turtle class
-import turtle
-from turtle import Turtle, Screen
+# #import turtle
+# #from turtle module import Turtle class
+# import turtle
+# from turtle import Turtle, Screen
+#
+#
+# #tapped into turtl object and called class (blue C icon) Turtle (capitalized to indicate class).
+# # Then save the class into an object called timmy
+# #timmy = turtle.Turtle()
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+#
+
+from prettytable import PrettyTable
+table = PrettyTable()
 
 
-#tapped into turtl object and called class (blue C icon) Turtle (capitalized to indicate class).
-# Then save the class into an object called timmy
-#timmy = turtle.Turtle()
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table.align = "l"
+# print(table.align)
 
 
+print(table)
